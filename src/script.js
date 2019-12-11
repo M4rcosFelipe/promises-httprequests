c570d6c64b3d6464cade07c54aef86be1ce0e0bc
marcos <marcosfelipe399@hotmail.com>
2019-12-11 13:51:33 -0300

    erro 'this.setSpan is not a function' corrigido //contexto do this

--- src/script.js
+++ src/script.js
@@ -1,88 +1,107 @@
 const object={
 
-listEl:document.querySelector("#lista"),
-inputEl:document.querySelector("input[name=username]"),
-botao:document.querySelector("#botao"),
-repos:[],
+    listEl:document.querySelector("#lista"),
+    inputEl:document.querySelector("input[name=username]"),
+    botao:document.querySelector("#botao"),
+    repos:[],
+    alredyAdd:[],
 
+    clicar(){
+        this.setSpan("Carregando...")
+        this.adicionar()
+            .then((message)=>{
+                this.setSpan(message)
+                this.renderRepos()
+            })
+            .catch(erro=>{  
+                this.setSpan(erro)
+            })
+    },
 
-
-
-clicar(){
     
-    this.setSpan('carregando...')
-    this.adicionar()
-        .then(()=>{
-            this.setSpan()
-            this.renderRepos()
-        })
-        .catch(erro=>{
-            this.setSpan(erro)
-            console.log(erro)})
-},
 
+    adicionar(){
+        let user=this.inputEl.value
+        let URL=`https://api.github.com/users/${user}`
 
-adicionar(){
+        return new Promise((resolve,reject)=>{
+            const xhr=new XMLHttpRequest()
 
-    let user=this.inputEl.value
-    let URL=`https://api.github.com/users/${user}`
-    console.log(URL)
-    return new Promise((resolve,reject)=>{
-        const xhr=new XMLHttpRequest()
+            xhr.open("GET",URL)
+            xhr.send(null)
+            that=this
+            xhr.onreadystatechange=function(){
 
-        xhr.open("GET",URL)
-        xhr.send(null)
+                if(xhr.readyState===4){
+                    if(xhr.status===200){
+                       
+                        var resposta=(JSON.parse(xhr.responseText))
+                        
+                        const is=that.verify(resposta.login,that.alredyAdd)
+                       
 
-        xhr.onreadystatechange=function(){
-            if(xhr.readyState===4){
-                if(xhr.status===200){
-                    repos.push(JSON.parse(xhr.responseText))
-                    resolve()
-                }else{
-                    reject("O repositório não existe")
+                        if(is===true) {
+                            resolve("repositório ja adicionado")
+                            return
+
+                        }else{
+                            that.repos.push(resposta)
+                            that.alredyAdd.push(resposta.login)
+                            resolve()
+                        }
+                    }else{
+                        reject("O repositório não existe")
+                    }
                 }
             }
+        })
+    },
+
+
+    renderRepos(){
+        this.listEl.innerHTML=""
+        this.inputEl.value=""
+        for(let i=0;i<this.repos.length;i++){
+            var listItem=this.createListItem(this.repos[i])
+            this.listEl.appendChild(listItem)
         }
-    })
-},
+    },
+
+    createListItem(repo){
+        let li=document.createElement("li")
+        let link=document.createElement("a")
+        let strong=document.createElement("strong")
+        let img=document.createElement("img")
+
+        let linkContent=document.createTextNode("Acesse")
+        let strongContent=document.createTextNode(repo.login)
+
+        img.setAttribute("src",repo.avatar_url)
+        strong.appendChild(strongContent)
+        link.setAttribute("href",repo.html_url)
+        link.setAttribute("target","_blank")
+        link.appendChild(linkContent)
+
+        li.appendChild(img)
+        li.appendChild(strong)
+        li.appendChild(link)
+
+        return li
+    },
+
+    setSpan(message=""){
+        let span=document.querySelector(".temp_span")
+        span.innerText=message
+    },
 
 
-renderRepos(){
-    this.listEl.innerHTML=""
-    this.inputEl.value=""
-    for(let i=0;i<this.repos.length;i++){
-        var listItem=this.createListItem(this.repos[i])
-        this.listEl.appendChild(listItem)
-    }
-},
+    verify(username,list){
+        let alredy=list.find(item=>item===username)
+        return alredy?true:false 
+    },
 
-createListItem(repo){
-    let li=document.createElement("li")
-    let link=document.createElement("a")
-    let strong=document.createElement("strong")
-    let img=document.createElement("img")
-
-    let linkContent=document.createTextNode("Acesse")
-    let strongContent=document.createTextNode(repo.login)
-
-    img.setAttribute("src",repo.avatar_url)
-    strong.appendChild(strongContent)
-    link.setAttribute("href",repo.html_url)
-    link.setAttribute("target","_blank")
-    link.appendChild(linkContent)
-
-    li.appendChild(img)
-    li.appendChild(strong)
-    li.appendChild(link)
-
-    return li
-},
-
-setSpan(message=""){
-    let span=document.querySelector(".temp_span")
-    span.innerText=message
-},
 
 }//end object
 
-botao.onclick=object.clicar
+
+object.botao.onclick=object.clicar.bind(object)
